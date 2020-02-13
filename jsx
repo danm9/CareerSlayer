ac12b56d80ee867d0c91dae03a5eae19b4b0392e
Daniel Minahan <dminahan@taxslayer.com>
2020-02-13 15:00:04 -0500

    clean up

--- jsx
+++
@@ -1,4 +0,0 @@
-M	src/components/app.js
-M	src/components/capabilityitem/index.js
-M	src/components/sidebar/index.js
-M	src/data/ladder.json
--- src/components/about/index.js
+++
@@ -1,9 +0,0 @@
-/*import { h, Component } from "preact";
-import Style from "./style.css";
-import source from "../../data/ladder.json";
-
-const About = () => {
-  return <div />;
-};
-
-export default About;*/

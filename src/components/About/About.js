04d573a79886ff7a1c6c0cccb663fcdfd705e15a
Daniel Minahan <dminahan@taxslayer.com>
2020-02-13 16:47:47 -0500

    working on About

---
+++ src/components/About/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Component } from 'react';
+
+class About extends Component {
+    render() {
+        return(
+            <div>
+                <h2>Hello World</h2>
+            </div>
+        )
+    }
+};
+
+export default About;
---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import About from './About/About';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <About />
+      </header>
+    </div>
+  );
+}
+
+export default App;

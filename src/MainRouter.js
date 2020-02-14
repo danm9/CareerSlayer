0b2521ca94529ffb3683c74f8a7830205d3316d1
Daniel Minahan <dminahan@taxslayer.com>
2020-02-14 13:14:37 -0500

    updates

--- src/MainRouter.js
+++ src/MainRouter.js
@@ -8,3 +8,19 @@ import About from './user/About';
 //import Library from './user/Library';
 //import Profile from './user/Profile';
 //import Users from './user/users';
+
+const MainRouter = () => (
+    <div>
+        <Menu />
+        <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/users" component={Users} />
+            <Route exact path="/signup" component={Signup} />
+            <Route exact path="/signin" component={Signin} />
+            <Route exact path="/user/edit:userId" component={EditProfile} />
+            <Route exact path="/user/:userId" component={Profile} />
+        </Switch>
+    </div>
+);
+
+export default MainRouter;
